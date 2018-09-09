c7869679b9fdef2c3fd15ebdc6606a8894911f4e
huydx <xuanhuy.do@linecorp.com>
2018-09-09 21:09:03 +0900

    use bootstrap-vue

---
+++ view/src/components/RequestLog.vue
@@ -0,0 +1,25 @@
+<template>
+  <div>
+    <b-table striped hover :items="items"></b-table>
+    <b-pagination size="sm" :total-rows="100" v-model="currentPage" :per-page="10">
+    </b-pagination>
+  </div>
+</template>
+
+<script>
+  const items = [
+    { isActive: true, age: 40, first_name: 'Dickerson', last_name: 'Macdonald' },
+    { isActive: false, age: 21, first_name: 'Larsen', last_name: 'Shaw' },
+    { isActive: false, age: 89, first_name: 'Geneva', last_name: 'Wilson' },
+    { isActive: true, age: 38, first_name: 'Jami', last_name: 'Carney' }
+  ]
+
+  export default {
+    data () {
+      return {
+        items: items,
+        currentPage: 3
+      }
+    }
+  }
+</script>
--- view/src/router/index.js
+++ view/src/router/index.js
@@ -1,15 +1,17 @@
 import Vue from 'vue'
 import Router from 'vue-router'
-import HelloWorld from '@/components/HelloWorld'
+import RequestLog from '@/components/RequestLog'
+import BootstrapVue from 'bootstrap-vue'
 
 Vue.use(Router)
+Vue.use(BootstrapVue)
 
 export default new Router({
   routes: [
     {
       path: '/',
-      name: 'HelloWorld',
-      component: HelloWorld
+      name: 'RequestLog',
+      component: RequestLog
     }
   ]
 })
